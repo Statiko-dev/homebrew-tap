e15e10a270204913acdbfa905ab38657637e4196
ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
2020-06-13 14:14:45 -0700

    Trying again

--- Formula/stkcli.rb
+++ Formula/stkcli.rb
@@ -1,25 +1,26 @@
 class Stkcli < Formula
-	desc "CLI to manage Statiko nodes and apps"
-	homepage "https://github.com/Statiko-dev/stkcli"
-	url "https://github.com/Statiko-dev/stkcli/archive/v0.5.0-beta.3.tar.gz"
+  desc "CLI to manage Statiko nodes and apps"
+  homepage "https://github.com/Statiko-dev/stkcli"
+  url "https://github.com/Statiko-dev/stkcli/archive/v0.5.0-beta.3.tar.gz"
   sha256 "f78dbabd71efca16a26e351434447ef99eff574cee9034232388f8afb40268d8"
 
-	depends_on "go" => ["1.13", :build]
+  depends_on "go" => ["1.13", :build]
 
-	def install
-		ENV["GOPATH"] = buildpath
-		ENV["GO111MODULE"] = "on"
-		ENV["CGO_ENABLED"] = "0"
-		ENV["PATH"] = "#{ENV["PATH"]}:#{buildpath}/bin"
-		(buildpath/"src/github.com/Statiko-dev/stkcli").install buildpath.children
-		cd "src/github.com/Statiko-dev/stkcli" do
+  def install
+    ENV["GOPATH"] = buildpath
+    ENV["GO111MODULE"] = "on"
+    ENV["CGO_ENABLED"] = "0"
+    ENV["PATH"] = "#{ENV["PATH"]}:#{buildpath}/bin"
+    (buildpath/"src/github.com/Statiko-dev/stkcli").install buildpath.children
+    cd "src/github.com/Statiko-dev/stkcli" do
       system "go", "build", "-ldflags",
-        "-X github.com/statiko-dev/stkcli/buildinfo.BuildID=v0.5.0-beta.3 -X github.com/statiko-dev/stkcli/buildinfo.CommitHash=brew",
+        "-X github.com/statiko-dev/stkcli/buildinfo.BuildID=v0.5.0-beta.3" +
+        "-X github.com/statiko-dev/stkcli/buildinfo.CommitHash=brew",
         "-o", bin/"stkcli", "."
-		end
-	end
+    end
+  end
 
-	test do
-		assert_match /stkcli Build ID: v0\.5\.0-beta\.3/, shell_output("#{bin}/stkclli version")
-	end
+  test do
+    assert_match /stkcli Build ID: v0\.5\.0-beta\.3/, shell_output("#{bin}/stkclli version")
+  end
 end
