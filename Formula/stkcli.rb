6b02feda17fd4364103507a94f72572a6c1c2315
ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
2020-06-13 14:01:09 -0700

    Fixed syntax errors

--- Formula/stkcli.rb
+++ Formula/stkcli.rb
@@ -1,23 +1,25 @@
 class Stkcli < Formula
-    desc "A CLI to manage Statiko nodes and apps"
-    homepage "https://github.com/Statiko-dev/stkcli"
-    url "https://github.com/Statiko-dev/stkcli/archive/v0.5.0-beta.3.tar.gz"
-    sha256 "f78dbabd71efca16a26e351434447ef99eff574cee9034232388f8afb40268d8"
-  
-    depends_on "go" => ["1.13", :build]
-  
-    def install
-        ENV["GOPATH"] = buildpath
-        ENV["GO111MODULE"] = "on"
-        ENV["CGO_ENABLED"] = "0"
-        ENV["PATH"] = "#{ENV["PATH"]}:#{buildpath}/bin"
-        (buildpath/"src/github.com/Statiko-dev/stkcli").install buildpath.children
-        cd "src/github.com/Statiko-dev/stkcli" do
-            system "go", "build", "-ldflags", "-X github.com/statiko-dev/stkcli/buildinfo.BuildID=v0.5.0-beta.3 -X github.com/statiko-dev/stkcli/buildinfo.CommitHash=brew", "-o", bin/"stkcli", "."
-        end
-    end
-  
-    test do
-        assert_match /stkcli Build ID\: v0\.5\.0\-beta\.3/, shell_output("#{bin}/stkclli version", 0)
-    end
+	desc "CLI to manage Statiko nodes and apps"
+	homepage "https://github.com/Statiko-dev/stkcli"
+	url "https://github.com/Statiko-dev/stkcli/archive/v0.5.0-beta.3.tar.gz"
+  sha256 "f78dbabd71efca16a26e351434447ef99eff574cee9034232388f8afb40268d8"
+
+	depends_on "go" => ["1.13", :build]
+
+	def install
+		ENV["GOPATH"] = buildpath
+		ENV["GO111MODULE"] = "on"
+		ENV["CGO_ENABLED"] = "0"
+		ENV["PATH"] = "#{ENV["PATH"]}:#{buildpath}/bin"
+		(buildpath/"src/github.com/Statiko-dev/stkcli").install buildpath.children
+		cd "src/github.com/Statiko-dev/stkcli" do
+      system "go", "build", "-ldflags",
+        "-X github.com/statiko-dev/stkcli/buildinfo.BuildID=v0.5.0-beta.3 -X github.com/statiko-dev/stkcli/buildinfo.CommitHash=brew",
+        "-o", bin/"stkcli", "."
+		end
+	end
+
+	test do
+		assert_match /stkcli Build ID: v0\.5\.0-beta\.3/, shell_output("#{bin}/stkclli version")
+	end
 end
